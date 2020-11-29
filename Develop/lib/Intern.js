af12a7de37d840342804bb13b994ac754fb8b689
Alice Golter <inkflight@gmail.com>
2020-11-29 10:41:46 -0600

    initial files/templates

---
+++ Develop/lib/Intern.js
@@ -0,0 +1,11 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+// * name
+// * id
+// * email
+// * getName()
+// * getId()
+// * getEmail()
+// * school 
+// * getSchool()
+// * getRole() // Overridden to return 'Intern'
